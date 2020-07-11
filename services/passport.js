5a8d81daec919e1f0c814cb556a95ae8917dcd2c
mikechoi1 <befuddledmike@gmail.com>
2020-07-10 20:17:01 -0700

    added production keys

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-//require db
 const keys = require('../config/keys');
 
 passport.use(new GoogleStrategy({
@@ -8,7 +7,7 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    // console.log('access token:', accessToken);
-    // console.log('refresh token:', refreshToken);
-    // console.log('profile:', profile);
+    console.log('access token:', accessToken);
+    console.log('refresh token:', refreshToken);
+    console.log('profile:', profile);
 }));
