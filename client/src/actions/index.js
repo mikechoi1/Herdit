4aabad68d905f8f5125ea2dbb400a10fb7eaf7bc
mikechoi1 <befuddledmike@gmail.com>
2020-10-23 19:41:12 -0700

    added action creator for retrieving posts

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_POSTS } from './types';
 
 //if redux-thunk sees we return a function instead of an object, the functions gets called with 'dispatch' argument
 export const fetchUser = () => async dispatch => {
-        //dev: uses proxy to use backend server instead of client server
-        //prod: no client server so just uses backend server
-        const res = await axios.get('/api/current_user');
-        dispatch({ type: FETCH_USER, payload: res.data });
-    }
+    //dev: uses proxy to use backend server instead of client server
+    //prod: no client server so just uses backend server
+    const res = await axios.get('/api/current_user');
+    dispatch({ type: FETCH_USER, payload: res.data });
+}
+
+export const fetchPosts = () => async dispatch => {
+    const res = await axios.get('/api/posts');
+    dispatch({ type: FETCH_POSTS, payload: res.data });
+}
